b1410629234e67bcea965f38fc6d8682ce77b8b2
wassupvin <kevinayp10@gmail.com>
2026-05-24 17:58:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -140,19 +140,19 @@ client.on("interactionCreate", async (interaction) => {
         { name: "XP Needed", value: neededXP.toLocaleString() },
         {
           name: "Pack 1",
-          value: `${results[0].amount}x (${results[0].cost} :dl:1508030424444702772)`
+          value: `${results[0].amount}x (${results[0].cost} :dl:)`
         },
         {
           name: "Pack 2",
-          value: `${results[1].amount}x (${results[1].cost} :dl:1508030424444702772)`
+          value: `${results[1].amount}x (${results[1].cost} :dl:)`
         },
         {
           name: "Pack 3",
-          value: `${results[2].amount}x (${results[2].cost} :dl:1508030424444702772)`
+          value: `${results[2].amount}x (${results[2].cost} :dl:)`
         },
         {
           name: "✅ Best Choice",
-          value: `${best.name} (${best.cost} :dl:1508030424444702772)`
+          value: `${best.name} (${best.cost} :dl:)`
         }
       );
 
